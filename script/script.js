98fbbbd40c657495f7c43626aab690a52bfbbbc3
ksuxaprokopova <111523946+ksuxaprokopova@users.noreply.github.com>
2022-10-15 20:11:15 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,4 +1,29 @@
 $('.products_top').parallax({imageSrc: '../img/products/fon_top.png',
 speed: 0.01
 });
+
+$("#button").on("click", function(e){
+    e.preventDefault();
+
+    let name = $("#name").val().trim();
+    let email = $("#email").val().trim();
+
+    if(name ==""){
+        $("#error_name").text("Enter your name")
+    }else if(email ==""){
+        $("#error_email").text("Enter your e-mail")
+    }
+
+    $.ajax({
+        url: "ajax/telegram.php",
+        type: "POST",
+        cache: false,
+        data: {"email": email, "name": name},
+        dataType: "html",
+        success: function(){
+            $("#name").val("");
+            $("#email").val("")
+        }
+    })
+})
    
